c8cb752df1ea0c3e97d85267366df3393cf6bb98
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-09-11 16:11:02 +0300

    tour datas added the pages

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -7,28 +7,21 @@ export default class Tour extends Component {
     return (
       <article className="tour">
         <div className="img-container">
-          <img
-            width={200}
-            src="https://images.pexels.com/photos/27810045/pexels-photo-27810045/free-photo-of-siyah-ve-beyaz-doga-bahce-fotografcilik.jpeg?auto=compress&cs=tinysrgb&w=800&lazy=load"
-            alt="picture1"
-          />
+          <img width={200} src={img} alt="picture1" />
           <span className="close-btn">
             <i className="fas fa-window-close" />
           </span>
         </div>
         <div className="tour-info">
-          <h3>City</h3>
-          <h4>Name</h4>
+          <h3>{city}</h3>
+          <h4>{name}</h4>
           <h5>
             info{" "}
             <span>
-              <i className="fas fa-caret-square-down"></i>
+              <i className="fas fa-caret-square-down" />
             </span>
           </h5>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam,
-            dignissimos.
-          </p>
+          <p>{info}</p>
         </div>
       </article>
     );
